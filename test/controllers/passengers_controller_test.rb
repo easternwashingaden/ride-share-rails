ef28860bb18ff5097eaec819ffaba563716753cf
peachmakkoli <sara.lee.higgins@gmail.com>
2020-05-08 14:58:29 -0700

    adds tests for index action

--- test/controllers/passengers_controller_test.rb
+++ test/controllers/passengers_controller_test.rb
@@ -2,7 +2,24 @@ require "test_helper"
 
 describe PassengersController do
   describe "index" do
-    # Your tests go here
+    before do
+      @passenger = Passenger.create(
+        name: "Lak Mok",
+        phone_num: "(555) 555-5555"
+      )
+    end
+
+    it "responds with success when there are many passengers saved" do
+      get passengers_path
+      must_respond_with :success
+    end
+
+    it "responds with success when there are no passengers saved" do
+      @passenger.destroy
+
+      get passengers_path
+      must_respond_with :success
+    end
   end
 
   describe "show" do
