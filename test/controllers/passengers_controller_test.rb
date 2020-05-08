1fca7bc65d161b37796f734006cbc42ce029477e
peachmakkoli <sara.lee.higgins@gmail.com>
2020-05-08 16:17:38 -0700

    adds tests for passengers#create action

--- test/controllers/passengers_controller_test.rb
+++ test/controllers/passengers_controller_test.rb
@@ -42,7 +42,45 @@ describe PassengersController do
   end
 
   describe "create" do
-    # Your tests go here
+    it "can create a new passenger with valid information accurately, and redirect" do
+      # Arrange
+      passenger_hash = {
+        passenger: {
+          name: "Lee H",
+          phone_num: "(555) 555-5556"
+        },
+      }
+
+      # Act-Assert
+      expect {
+        post passengers_path, params: passenger_hash
+      }.must_differ "Passenger.count", 1
+
+      # Assert
+      new_passenger = Passenger.find_by(name: passenger_hash[:passenger][:name])
+      expect(new_passenger.name).must_equal passenger_hash[:passenger][:name]
+      expect(new_passenger.phone_num).must_equal passenger_hash[:passenger][:phone_num]
+      
+      must_redirect_to passenger_path(new_passenger.id)
+    end
+
+    it "does not create a passenger if the form data violates passenger validations, and responds with a 204 status" do
+      # Arrange
+      passenger_hash = {
+        passenger: {
+          name: "Lee H",
+          phone_num: nil
+        },
+      }
+
+      # Act-Assert
+      expect {
+        post passengers_path, params: passenger_hash
+      }.wont_differ "Passenger.count"
+
+      # Assert
+      must_respond_with :no_content
+    end
   end
 
   describe "edit" do
