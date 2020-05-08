a0e1d3b244a0176f179e81c2690641294055681c
peachmakkoli <sara.lee.higgins@gmail.com>
2020-05-08 15:51:21 -0700

    adds test for passengers#new

--- test/controllers/passengers_controller_test.rb
+++ test/controllers/passengers_controller_test.rb
@@ -35,7 +35,10 @@ describe PassengersController do
   end
 
   describe "new" do
-    # Your tests go here
+    it "responds with success" do
+      get new_passenger_path
+      must_respond_with :success
+    end
   end
 
   describe "create" do
