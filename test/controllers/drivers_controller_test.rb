05daf858bf67987a78bd5774dee30e682123c836
Tithvorlak Mok <tithvorlakmok@gmail.com>
2020-05-08 17:08:26 -0700

    Added tests for index, show, create, edit, updat)

--- test/controllers/drivers_controller_test.rb
+++ test/controllers/drivers_controller_test.rb
@@ -38,10 +38,12 @@ describe DriversController do
     it "responds with success when showing an existing valid driver" do
       # Arrange
       # Ensure that there is a driver saved
-
+      
       # Act
+      get driver_path(driver.id)
 
       # Assert
+      must_respond_with :success
 
     end
 
@@ -50,29 +52,52 @@ describe DriversController do
       # Ensure that there is an id that points to no driver
 
       # Act
+      get driver_path(-1)
 
       # Assert
+      must_respond_with :redirect # Here, we can either redirect or show a sucess message
 
     end
   end
 
   describe "new" do
     it "responds with success" do
+    # Act
+    get new_driver_path
     end
+    # Assert 
+    must_respond_with :sucess
   end
 
   describe "create" do
     it "can create a new driver with valid information accurately, and redirect" do
       # Arrange
       # Set up the form data
+      driver_hash = {
+        driver: {
+          name: "new driver",
+          vin: "BAJDKSH124",
+          available: true
+        },
+      }
 
       # Act-Assert
       # Ensure that there is a change of 1 in Driver.count
-
+      expect {
+        post drivers_path, params: driver_hash
+      }.must_change "Driver.count", 1
       # Assert
       # Find the newly created Driver, and check that all its attributes match what was given in the form data
+      
+      new_driver = Driver.find_by(name: driver_hash[:driver][:name])
+      expect(new_driver.name).must_equal driver_hash[:driver][:name]
+      expect(new_driver.vin).must_equal driver_hash[:driver][:vin]
+      expect(new_driver.available).must_equal driver_hash[:driver][:available]
+      
       # Check that the controller redirected the user
-
+      must_respond_with: redirect
+      must_redirect_to driver_path(new_driver.id) #
+    
     end
 
     it "does not create a driver if the form data violates Driver validations, and responds with a redirect" do
@@ -93,21 +118,23 @@ describe DriversController do
     it "responds with success when getting the edit page for an existing, valid driver" do
       # Arrange
       # Ensure there is an existing driver saved
-
+      
       # Act
+      get edit_driver_path(driver.id)
 
       # Assert
+      must_respond_with :success
 
     end
 
     it "responds with redirect when getting the edit page for a non-existing driver" do
       # Arrange
       # Ensure there is an invalid id that points to no driver
-
+      get edit_task_path(-1)
+    
       # Act
-
       # Assert
-
+      must_respond_with :not_found
     end
   end
 
@@ -117,14 +144,32 @@ describe DriversController do
       # Ensure there is an existing driver saved
       # Assign the existing driver's id to a local variable
       # Set up the form data
+      before do
+        Driver.create(name: " new driver", vin: "FJSKDJ12", available: true)
+        @params_hash = {
+          driver: {
+            name: "Lee Lak",
+            vin: "JDLDF1245",
+            available: true
+          }
+        }
+      end
 
       # Act-Assert
       # Ensure that there is no change in Driver.count
+      updated_driver = Driver.first
+      expect{
+        patch driver_path(update_driver.id), params: @params_hash
+      }.must_differ "Driver.count", 0
+    
 
       # Assert
       # Use the local variable of an existing driver's id to find the driver again, and check that its attributes are updated
       # Check that the controller redirected the user
-
+      must_redirect_to driver_path(updated_driver.id)
+      expect(Driver.first.name).must_equal @params_hash[:driver][:name]
+      expect(Driver.first.vin).must_equal @params_hash[:driver][:vin]
+      expect(Driver.first.available).must_equal @params_hash[:driver][:available]
     end
 
     it "does not update any driver if given an invalid id, and responds with a 404" do
@@ -134,7 +179,7 @@ describe DriversController do
 
       # Act-Assert
       # Ensure that there is no change in Driver.count
-
+      
       # Assert
       # Check that the controller gave back a 404
 
