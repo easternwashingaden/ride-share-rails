18f10d8b4f01816f93a455e05cadf27c539dfa80
peachmakkoli <sara.lee.higgins@gmail.com>
2020-05-09 16:34:27 -0700

    adds trips#show and #create actions

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,14 +1,30 @@
 class TripsController < ApplicationController
-  def index
-  end
-
   def show
-  end
-
-  def new
+    @trip = Trip.find_by(id: params[:id])
+    head :not_found if @trip.nil?
+    return
   end
 
   def create
+    driver = Driver.find_by(available: true)
+    
+    @trip = Trip.new(
+      passenger_id: params[:id],
+      driver_id: driver.id,
+      date: Date.today,
+      cost: rand(1..5000), # set cost to random number
+      rating: nil # set rating to nil
+    )
+
+    if @trip.save
+      # flash[:success] = "Trip added successfully"
+      redirect_to trip_path(@trip)
+      return
+    else
+      # flash.now[:error] = "Something went wrong. Trip not added"
+      redirect_to passenger_path(params[:id]), status: :bad_request
+      return
+    end
   end
 
   def edit
@@ -19,4 +35,10 @@ class TripsController < ApplicationController
 
   def destroy
   end 
+
+  private 
+
+  def trip_params
+    return params.require(:trip).permit(:date, :cost, :rating)
+  end
 end
