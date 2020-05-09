bd80e4773e291759f4784afb59e29473fa231e11
Tithvorlak Mok <tithvorlakmok@gmail.com>
2020-05-08 22:30:18 -0700

    created show action and passed the test

--- app/controllers/drivers_controller.rb
+++ app/controllers/drivers_controller.rb
@@ -1,12 +1,19 @@
 class DriversController < ApplicationController
   def index
-    @drivers = Driver.all
+    @drivers = Driver.all.order("id")
   end
 
   def show
+    driver_id = params[:id]
+    @driver = Driver.find_by(id: driver_id)
+    if @driver.nil?
+      head :not_found
+      return
+    end
   end
 
   def new
+    @driver = Driver.new
   end
 
   def create
