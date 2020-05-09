381ebe8498e8eca07fff95302c9571617ee096ed
Tithvorlak Mok <tithvorlakmok@gmail.com>
2020-05-08 23:13:51 -0700

    Added edit, update, destroy actions but still failed one test

--- app/controllers/drivers_controller.rb
+++ app/controllers/drivers_controller.rb
@@ -17,14 +17,58 @@ class DriversController < ApplicationController
   end
 
   def create
+    @driver = Driver.new(driver_params)
+    if @driver.save
+      # flash[:success] = "Driver added successfully"
+      redirect_to driver_path(@driver)
+      return
+    else
+      # flash.now[:error] = "Something went wrong. Driver not added"
+      render :new, status: :bad_request
+      return
+    end
   end
 
   def edit
+    @driver = Driver.find_by(id: params[:id])
+    if @driver.nil?
+      redirect_to drivers_path
+      return
+    end
   end
 
   def update
+    @driver = Driver.find_by(id: params[:id])
+    
+    if @driver.nil?
+      head :not_found
+      return
+    elsif @driver.update(driver_params)
+      redirect_to driver_path(@driver)
+      return
+    else 
+      render :edit, status: :bad_request
+      return
+    end
   end
-  
+
   def destroy
+    @driver = Driver.find_by(id: params[:id])
+    
+    if @driver.nil?
+      head :not_found
+      return
+    else
+      @driver.destroy
+      redirect_to drivers_path
+      return
+    end
+  end 
+
+
+  private
+  def driver_params
+    return params.require(:driver).permit(:name, :vin)
   end
+
 end
