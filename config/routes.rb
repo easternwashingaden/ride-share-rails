9331965bf99149b3cb8a1352031f088756bc8f1c
peachmakkoli <sara.lee.higgins@gmail.com>
2020-05-09 15:44:09 -0700

    adds nested route for creating trips, routes for edit, show, update, and destroy

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
-  root to: "homepages#index"
-  resources :trips
-  resources :passengers
-  resources :drivers
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
+  root to: "homepages#index"
+  resources :passengers
+
+  resources :drivers
   patch '/drivers/:id/toggle_available', to: 'drivers#toggle_available', as: 'driver_available'
+  
+  post "passengers/:id/trips", to: "trips#create", as: "trips"
+  get "trips/:id/edit", to: "trips#edit", as: "edit_trip"
+  get "trips/:id", to: "trips#show", as: "trip"
+  patch "trips/:id", to: "trips#update" 
+  delete "trips/:id", to: "trips#destroy"
 end
