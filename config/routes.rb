bec78a271ca7ac159590a7f06cf7c69be13723ca
Tithvorlak Mok <tithvorlakmok@gmail.com>
2020-05-09 12:12:19 -0700

    Added make_available method and added vin strong params. Also updated the create method, set available to true when a driver is created

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,6 @@ Rails.application.routes.draw do
   resources :passengers
   resources :drivers
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  patch '/drivers/:id/make_available', to: 'drivers#make_available', as: 'driver_available'
 end
