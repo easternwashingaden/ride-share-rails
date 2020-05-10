da0102c264a553720fee0aa5f506ba03dda98213
Tithvorlak Mok <tithvorlakmok@gmail.com>
2020-05-10 10:22:31 -0700

    Wrote some tests for trip_test

--- test/models/trip_test.rb
+++ test/models/trip_test.rb
@@ -1,16 +1,55 @@
 require "test_helper"
 
 describe Trip do
+  let (:driver) {
+    Driver.create(
+      name: "Lee H", 
+      vin: "FJSKDJ12",
+      available: false
+    )
+  }
+  
+  let (:passenger) {
+    Passenger.create(
+      name: "Lak Mok",
+      phone_num: "(555) 555-5555"
+    )
+  }
+
+  let (:new_trip) {
+    Trip.create(
+      driver_id: driver.id,
+      passenger_id: passenger.id, 
+      date: Date.today, 
+      cost: rand(1..5000),
+      rating: nil,
+    )
+  }
+
   it "can be instantiated" do
-    # Your code here
+    expect(new_trip.valid?).must_equal true
   end
 
   it "will have the required fields" do
-    # Your code here
+    new_trip.save
+    trip = Trip.first
+    [:driver_id, :passenger_id, :date, :cost, :rating].each do |field|
+
+      # Assert
+      expect(trip).must_respond_to field
   end
+end
 
   describe "relationships" do
-    # Your tests go here
+    it "trip can link to only one driver and passenger" do
+      # Arrange
+      new_trip.save
+      
+      # Assert
+      expect(new_trip.driver.count).must_equal 1
+      expect(new_trip.passenger.count).must_equal 1
+   
+    end
   end
 
   describe "validations" do
