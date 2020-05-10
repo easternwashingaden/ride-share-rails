ba7576c08bc296be1063019e30e2ee6c19bd0f2b
peachmakkoli <sara.lee.higgins@gmail.com>
2020-05-10 01:08:56 -0700

    reconfigures nested routing for trips#create

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,13 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   root to: "homepages#index"
-  resources :passengers
 
   resources :drivers
   patch '/drivers/:id/toggle_available', to: 'drivers#toggle_available', as: 'driver_available'
   
-  post "passengers/:id/trips", to: "trips#create", as: "trips"
+  resources :passengers do
+    resources :trips, only: [:create]
+  end
+  
   resources :trips, except: [:index, :new, :create]
 end
