d9169c908dda40c969cb1d5f8e8a8c3651f0f397
peachmakkoli <sara.lee.higgins@gmail.com>
2020-05-11 12:08:18 -0700

    defines passenger#total_charges method

--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -2,4 +2,17 @@ class Passenger < ApplicationRecord
   has_many :trips
   validates :name, presence: true
   validates :phone_num, presence: true
+
+  def request_ride
+
+  end
+
+  def complete_trip
+  
+  end
+
+  def total_charges
+    trips = Trip.where(passenger_id: self.id)
+    return trips.empty? ? 0 : (trips.map { |trip| trip.cost }.sum)
+  end
 end
