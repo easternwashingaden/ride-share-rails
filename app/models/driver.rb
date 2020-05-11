ed607701b3f0e9be9f6e274e5fd9d2aa8dfb8556
Tithvorlak Mok <tithvorlakmok@gmail.com>
2020-05-11 14:05:10 -0700

    Implemented get_total_earnings for each driver

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -2,4 +2,10 @@ class Driver < ApplicationRecord
   has_many :trips
   validates :name, presence: true
   validates :vin, presence: true
+
+  def get_total_earnings
+    trips = Trip.where(driver_id: self.id)
+    return trips.empty? ? 0 : (trips.map { |trip| trip.cost }.sum)
+  end
+
 end
