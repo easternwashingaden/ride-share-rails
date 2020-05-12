e3583f1b38a0eeaf6f20bf17cea0b6d294e1f629
peachmakkoli <sara.lee.higgins@gmail.com>
2020-05-12 10:03:22 -0700

    adds driver and passenger methods for relationships

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -4,9 +4,13 @@ class Trip < ApplicationRecord
   belongs_to :driver
   belongs_to :passenger
 
-  validates :driver_id, :passenger_id, :date, :cost, presence: true
-  validates :rating, :inclusion => {:in => [nil, 1, 2, 3, 4, 5]}
-  validates :cost, numericality: { only_integer: true }
+  def driver
+    return Driver.find_by(id: self.driver_id)
+  end
+
+  def passenger
+    return Passenger.find_by(id: self.passenger_id)
+  end
 
   def cost_to_currency
     return number_to_currency(self.cost)
