ab068083a3e04e834c53a36570ef84112f0beda4
peachmakkoli <sara.lee.higgins@gmail.com>
2020-05-12 20:38:52 -0700

    cleans up driver model tests

--- test/models/driver_test.rb
+++ test/models/driver_test.rb
@@ -71,51 +71,51 @@ describe Driver do
 
   # Tests for methods you create should go here
   describe "total_earnings" do
-    it " The total earnings should be 0 if the driver doesn't have any trip" do
+    it "returns 0 if the driver doesn't have any trips" do
       # Arrange 
-      # No trip is created
+      new_driver.save
 
       # Assert 
       expect(new_driver.get_total_earnings).must_equal 0
     end
 
-    it " The total earnings should be sum of all trips that the driver gets 80% of the trip cost after a fee of $1.65" do
+    it "accurately calculates total earnings if the driver has trips (80% of the trip cost after a fee of $1.65)" do
       # Arrange
       new_driver.save
       new_passenger.save
-      trip_1 = Trip.create(driver_id: new_driver.id, passenger_id: new_passenger.id, date: Date.today, rating: 5, cost: 250)
-      trip_2 = Trip.create(driver_id: new_driver.id, passenger_id: new_passenger.id, date: Date.today, rating: 3, cost: 100)
-      trip_3 = Trip.create(driver_id: new_driver.id, passenger_id: new_passenger.id, date: Date.today, rating: 1, cost: 168)
+      trip_1 = Trip.create!(driver_id: new_driver.id, passenger_id: new_passenger.id, date: Date.today, rating: 5, cost: 251.65)
+      trip_2 = Trip.create!(driver_id: new_driver.id, passenger_id: new_passenger.id, date: Date.today, rating: 3, cost: 101.65)
+      trip_3 = Trip.create!(driver_id: new_driver.id, passenger_id: new_passenger.id, date: Date.today, rating: 1, cost: 151.65)
       
       trips = Trip.where(driver_id: new_driver.id)
-      
+    
       # Assert
       expect(trips.count).must_equal 3
-      expect(new_driver.get_total_earnings).must_equal 410.44.to_s
+      expect(new_driver.get_total_earnings).must_equal 400.0
     end
 
     describe "average_rating" do
-      it " The average rating should be 0 if the driver doesn't have any trip" do
+      it "returns 0 if the driver doesn't have any trips" do
         # Arrange 
-        # No trip is created
+        new_driver.save
   
         # Assert 
         expect(new_driver.average_rating).must_equal 0
       end
   
-      it "The average rating should be sum of all ratings divided by the number of ratings" do
+      it "accurately calculates average rating if the driver has trips" do
         # Arrange
         new_driver.save
         new_passenger.save
         trip_1 = Trip.create(driver_id: new_driver.id, passenger_id: new_passenger.id, date: Date.today, rating: 5, cost: 250)
         trip_2 = Trip.create(driver_id: new_driver.id, passenger_id: new_passenger.id, date: Date.today, rating: 3, cost: 100)
-        trip_3 = Trip.create(driver_id: new_driver.id, passenger_id: new_passenger.id, date: Date.today, rating: 2, cost: 168)
+        trip_3 = Trip.create(driver_id: new_driver.id, passenger_id: new_passenger.id, date: Date.today, rating: 1, cost: 150)
         
         trips = Trip.where(driver_id: new_driver.id)
         
         # Assert
         expect(trips.count).must_equal 3
-        expect(new_driver.average_rating).must_equal "3.00"
+        expect(new_driver.average_rating).must_equal 3.0
       end
     end
 
